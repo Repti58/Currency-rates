08aa1ce011e44e40c0a9ac37e64e705137fc7584
repti85 <ids-reptile@bk.ru>
2022-09-15 17:13:39 +0300

    view buttons style

--- src/App.js
+++ src/App.js
@@ -67,12 +67,35 @@ function App() {
           <span className="view-buttons">
             <Link to="/tabular-view">
               <button type="button" className="btn btn-primary btn-sm">
-                <i className="bi bi-table"></i>
+                {/* < className="bi bi-list-ul"></> */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="20"
+                  fill="currentColor"
+                  class="bi bi-list-ul"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"
+                  />
+                </svg>
               </button>
             </Link>
             <Link to="/mosaic-view">
               <button type="button" className="btn btn-primary btn-sm">
-                <i className="bi bi-grid-fill"></i>
+                {/* <i className="bi bi-grid-fill"></i> */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="20"
+                  fill="currentColor"
+                  class="bi bi-grid-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z" />
+                </svg>
               </button>
             </Link>
           </span>
@@ -82,7 +105,7 @@ function App() {
         {!currencyItems[0] ? (
           <div className="no-data">За выбранный период данных не найдено</div>
         ) : (
-          <Routes>          
+          <Routes>
             <Route
               path="/mosaic-view"
               element={<MosaicView currencyItems={currencyItems} />}
