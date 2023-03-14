85622cb9f6250284a50c17976940ee33b1240c37
repti85 <ids-reptile@bk.ru>
2023-03-14 23:14:15 +0300

    add diagram.css

--- src/CurrencyList/TabularView/Diagram/Diagram.jsx
+++ src/CurrencyList/TabularView/Diagram/Diagram.jsx
@@ -6,58 +6,49 @@ import { useParams } from "react-router-dom";
 import { setDiagramData } from "../../../Redux/currencySlice";
 import "./Diagram.css";
 
-
 const Diagram = (props) => {
-  debugger
+  debugger;
   console.log(useParams());
   const { currencyCode } = useParams();
   const { currencyTicker } = useParams();
-  const { currencyName } = useParams();  
+  const { currencyName } = useParams();
 
   const dispatch = useDispatch();
-  
+
   let diagramData;
-  
 
   useEffect(() => {
-    
-    setDiagramData()
+    setDiagramData();
     const getDiagramData = async () => {
-      
-  
       try {
-        await axios     
+        await axios
           .get(
-            `http://localhost:3003/ratesDynamic?dateStart=12/02/2023&dateEnd=12.03.2023&currencyName=${currencyCode}`          
+            `http://localhost:3003/ratesDynamic?dateStart=12/02/2023&dateEnd=12.03.2023&currencyName=${currencyCode}`
           )
           .then((response) => {
-            
-            diagramData = response.data
+            diagramData = response.data;
             diagramData.unshift(["date", currencyTicker]);
           });
       } catch {}
-      
+
       dispatch(setDiagramData(diagramData));
     };
     getDiagramData();
-
   }, []);
-  return (
-    !props.diagramData ? (
-      <div className="loader"></div>
-    ) : (
-      <div>
-        <div className="currency-name">
-        {currencyName}
-        </div>
-    <Chart className="diagram"
-      chartType="LineChart"
-      data={props.diagramData}
-      width="100%"
-      height="400px"
-    />
+  return !props.diagramData ? (
+    <div className="loader"></div>
+  ) : (
+    <div>
+      <div className="currency-name">{currencyName}</div>
+      <div className="diagram">
+        <Chart          
+          chartType="LineChart"
+          data={props.diagramData}
+          width="100%"
+          height="400px"
+        />
+      </div>
     </div>
-    )
   );
 };
 export default Diagram;
